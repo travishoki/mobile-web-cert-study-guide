909a0d65a6b4487a4c4736143cb395536a0d6b90
Travis Hoki <travis.hoki@gmail.com>
2017-09-13 22:18:36 -0600

    adding to page structure

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const server = express();
 const port = 8080;
 
-// server.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-
 server.use(express.static(__dirname + '/public'));
 
 server.listen(port, function(err) {
